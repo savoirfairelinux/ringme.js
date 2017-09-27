3dde0dc616c15f877a3d258e9deef4c561395cfd
Samuel Sirois <samuel@sirois.info>
2017-09-27 19:32:12 -0400

    Fix copyright and license for function launchUri
    Thanks to @aaronk6 for accepting releasing this code under a
    compatible license (Expat License) with RingMe.js's license.
    
    See: https://gist.github.com/aaronk6/d801d750f14ac31845e8#gistcomment-2010734

--- src/ringme.js
+++ src/ringme.js
@@ -149,9 +149,28 @@ var RingMe = new function() {
     );
   }
 
-  // https://gist.github.com/aaronk6/d801d750f14ac31845e8
-  // this function is not under Savoir-faire Linux inc. copyright and license negociation
-  // for this code is ongoing here: https://gist.github.com/aaronk6/d801d750f14ac31845e8#gistcomment-1982506
+  /*!
+   * Copyright © 2015 aaronk6
+   * https://gist.github.com/aaronk6/d801d750f14ac31845e8
+   *
+   * Permission is hereby granted, free of charge, to any person obtaining a copy
+   * of this software and associated documentation files (the "Software"), to deal
+   * in the Software without restriction, including without limitation the rights
+   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   * copies of the Software, and to permit persons to whom the Software is
+   * furnished to do so, subject to the following conditions:
+   *
+   * The above copyright notice and this permission notice shall be included in all
+   * copies or substantial portions of the Software.
+   *
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+   */
   var _launchUri = function(uri, successCallback, noHandlerCallback, unknownCallback) {
     var res, parent, popup, iframe, timer, timeout, blurHandler, timeoutHandler, browser;
 
